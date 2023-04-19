964a5dd547f27e59bc758628f5ce0a5079af313d
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-04-19 19:40:49 -0300

    Acrescentado modulo para adicionar Aluno

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/bruxeiro/go_api_gin/database"
 	"github.com/bruxeiro/go_api_gin/models"
 	"github.com/gin-gonic/gin"
 )
@@ -14,5 +17,18 @@ func Saudacao(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
+
+}
+
+func CriarNovoAluno(c *gin.Context) {
+	var aluno models.Aluno
+	if err := c.ShouldBindJSON(&aluno); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+
+	}
+	database.DB.Create(&aluno)
+	c.JSON(http.StatusOK, aluno)
 
 }
