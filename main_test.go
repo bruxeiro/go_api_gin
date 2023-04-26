7c3a71cb977dd764434bba19b03542e4bd718a26
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-04-26 18:54:47 -0300

    Teste de requisição + Get interno

--- main_test.go
+++ main_test.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/bruxeiro/go_api_gin/controller"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
@@ -20,8 +23,11 @@ func TestStatusCodeSaudacaoParametro(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/eu", nil)
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz:":"E ai eu, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(mockDaResposta)
 
-	if resposta.Code != http.StatusOK {
-		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
-	}
 }
