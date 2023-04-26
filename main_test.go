af789fbd98c3edc197d1c254a5b2d1973e3650e5
Bruxeiro <gabrielrosabueno@hotmail.com>
2023-04-26 18:33:11 -0300

    Status verificado

--- main_test.go
+++ main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/bruxeiro/go_api_gin/controller"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,14 @@ func SetupDasRotasDeTeste() *gin.Engine {
 	return rotas
 }
 
-func TestFalhador(t *testing.T) {
-	t.Fatalf("teste falhou de proposito")
+func TestStatusCodeSaudacaoParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controller.Saudacao)
+	req, _ := http.NewRequest("GET", "/eu", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+
+	if resposta.Code != http.StatusOK {
+		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
+	}
 }
